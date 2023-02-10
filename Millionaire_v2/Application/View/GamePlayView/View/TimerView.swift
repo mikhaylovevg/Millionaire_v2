969f9ef0929c122fea981e15521384f977594905
Pavel Gri <zlibabaika@gmail.com>
2023-02-10 15:13:29 +0400

    add ext UIColor, create TimerView

---
+++ Millionaire_v2/Application/View/GamePlayView/View/TimerView.swift
@@ -0,0 +1,25 @@
+//
+//  TimerView.swift
+//  Millionaire_v2
+//
+//  Created by Павел Грицков on 10.02.23.
+//
+
+import UIKit
+
+class TimerView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        
+        backgroundColor = 
+        
+    }
+    
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Millionaire_v2/Helper/UIColor + ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor + ext.swift
+//  Millionaire_v2
+//
+//  Created by Павел Грицков on 10.02.23.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var red5: UIColor {
+        #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+    }
+    
+    static var red2: UIColor {
+        #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
+    }
+    
+}
