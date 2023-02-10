5713a90220eba3c41ceebc10dd4a6a103db7b2aa
mikhaylovevg <mikhaylov.e.s@gmail.com>
2023-02-10 11:12:52 +0300

    Initial Commit

---
+++ Millionaire_v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Millionaire_v2
+//
+//  Created by Evgeny Mikhaylov on 10.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
